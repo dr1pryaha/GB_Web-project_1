4aa32e0eec657386d32faa9c8a5c3eda69e49f4f
Anton Pryakhin <dr.pryah@gmail.com>
2022-09-18 16:49:31 +0300

    first commit

---
+++ src/main.js
@@ -0,0 +1,32 @@
+import { diffDates, diffToHtml } from "./datecalc.js"; // Импортируем 2 функции из файла src/datecalc.js.
+
+import { formatError } from "./utils.js"; // Импортируем функцию из файла src/utils.js
+
+const dateCalcForm = document.getElementById("datecalc");
+const dateCalcResult = document.getElementById("datecalc__result");
+dateCalcForm.addEventListener("submit", handleCalcDates);
+function handleCalcDates(event) {
+  dateCalcResult.innerHTML = "";
+  event.preventDefault();
+  let { firstDate, secondDate } = event.target.elements;
+  (firstDate = firstDate.value), (secondDate = secondDate.value);
+  if (firstDate && secondDate) {
+    const diff = diffDates(firstDate, secondDate); // Вычитаем промежуток между датами.
+
+    dateCalcResult.innerHTML = diffToHtml(diff); // Форматируем промежуток, как html и вставляем в DOM.
+  } else
+    dateCalcResult.innerHTML = formatError(
+      "Для расчета промежутка необходимо заполнить оба поля"
+    ); // Если одно из полей не заполнено (или оба), выводим ошибку
+}
+
+const checkbox = document.querySelector(".input-checkbox");
+// const checkboxField = document.querySelector(".field-checkbox");
+
+checkbox.addEventListener("change", function () {
+  if (this.checked) {
+    dateCalcForm.classList.add("hidden");
+  } else {
+    dateCalcForm.classList.add("hidden");
+  }
+});
